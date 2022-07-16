8c958957b31a2ae7f877dbc018792fedd1a61b66
haha258369 <thanhhagstt@gmail.com>
2022-07-16 18:49:50 +0700

    Lab 15

---
+++ src/test/java/api_learning/LaunchBrowser.java
@@ -0,0 +1,40 @@
+package api_learning;
+
+import org.apache.commons.exec.OS;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LaunchBrowser {
+
+    public static void main(String[] args) {
+        String currentProjectLocation = System.getProperty("user.dir");
+        String chromeDriverLocation = "";
+        if (OS.isFamilyWindows()){
+            chromeDriverLocation = currentProjectLocation + "/src/test/resources/driver/chromedriver.exe";
+        }
+
+        if (chromeDriverLocation.isEmpty()){
+            throw new IllegalArgumentException("Cannot detect OS type");
+        }
+
+        System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
+        webDriver.manage().window().maximize();
+        webDriver.get("https://google.com");
+
+        // For debugging
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        webDriver.quit();
+    }
+}
+
